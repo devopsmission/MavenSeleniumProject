75f274364514925be54f4a0e6371a54a5d222922
Champak <devopsmisson@gmail.com>
2022-02-12 06:26:21 +0000

    Initial comment

---
+++ src/main/java/TesingMaven/TesingMaven/App.java
@@ -0,0 +1,13 @@
+package TesingMaven.TesingMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
